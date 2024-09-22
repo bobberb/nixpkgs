a8023594941ab6b4283d61dab01a2492d0cbffef
Randy Eckenrode <randy@largeandhighquality.com>
2024-09-22 00:38:08 -0400

    darwin.libiconv: move back to darwin attrset and use mkAppleDerivation

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21282,7 +21282,7 @@ with pkgs;
         then libcCross
         else stdenv.cc.libc)
     else if stdenv.hostPlatform.isDarwin
-      then libiconv-darwin
+      then darwin.libiconv
     else libiconvReal;
 
   libcIconv = libc: let
